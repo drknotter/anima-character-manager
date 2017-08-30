ab143ffff8dfbe499bdb1b4d62cb3850864faa79
Patrick Plunkett <patplunkett@patplunkett-macbookpro.roam.corp.google.com>
2017-08-29 18:37:22 -0700

    Adding ki effect info.

--- js/new_ki_technique.js
+++ js/new_ki_technique.js
@@ -3,6 +3,9 @@ $( document ).ready(function() {
   $('#addSecondaryEffect').click(function(event) {
     addEffect($('#secondaryEffectGroup'), true);
   });
+  $('#addDisadvantage').click(function(event) {
+    addDisadvantage($('#disadvantageGroup'))
+  })
 });
 
 function addEffect(parent, removeable, id) {
@@ -15,17 +18,47 @@ function addEffect(parent, removeable, id) {
   effectContainer.find('.removeEffect').click(function(event) {
     effectContainer.remove();
   });
+  effectContainer.find('.effectSelector').change(function(event) {
+    if ($(this).val() in KiTechnique.Data.Effects) {
+      var data = {'key': $(this).val(), 'data': KiTechnique.Data.Effects[$(this).val()]};
+      effectContainer.find('.effectInfo').html(Mustache.render(Template.effectInfo, data));
+    } else {
+      effectContainer.find('.effectInfo').empty();
+    }
+  })
 }
 
 function addEffectOptions(select) {
-  select.append(Mustache.render(Template.effectOption, {
-    'key': "",
+  select.append(Mustache.render(Template.selectOption, {
+    'value': "",
     'name': ""
   }));
   for (let i in KiTechnique.Data.Effects) {
-    select.append(Mustache.render(Template.effectOption, {
-      'key': i,
+    select.append(Mustache.render(Template.selectOption, {
+      'value': i,
       'name': KiTechnique.Data.Effects[i].name
+    }));
+  }
+}
+
+function addDisadvantage(parent) {
+  var disadvantageContainer = $(Mustache.render(Template.disadvantage).replace(/\r?\n|\r/g,''));
+  parent.append(disadvantageContainer);
+  addDisadvantageOptions(disadvantageContainer.find('.disadvantageSelector'));
+  disadvantageContainer.find('.removeDisadvantage').click(function(event) {
+    disadvantageContainer.remove();
+  });
+}
+
+function addDisadvantageOptions(select) {
+  select.append(Mustache.render(Template.selectOption, {
+    'value': "",
+    'name': ""
+  }));
+  for (let i in KiTechnique.Data.Disadvantages) {
+    select.append(Mustache.render(Template.selectOption, {
+      'value': i,
+      'name': KiTechnique.Data.Disadvantages[i].name
     }));
   }
 }
--- js/new_ki_technique.templates.js
+++ js/new_ki_technique.templates.js
@@ -1,12 +1,36 @@
 Template = {};
 
 Template.effect = String.raw`
-<tr><td><div class="effectContainer" style="display:table; width:100%;">
+<tr><td><div class="effectContainer">
+<div style="display:table; width:100%;">
 <select {{#id}}id="{{id}}" {{/id}}class="effectSelector" style="display:table-cell; width:100%;"></select>
 {{#removeable}}<div class="space" style="display:table-cell; width:0.5rem;"/><div class="removeEffect" style="display:table-cell; width:1px;">Remove</div>{{/removeable}}
+</div>
+<div class="effectInfo">
+</div>
 </div></td></tr>
 `
 
-Template.effectOption = String.raw`
-<option value="{{key}}">{{name}}</option>
+Template.effectInfo = String.raw`
+{{data.description}}
+<table class="effectLevels">
+<tr><th class="radio"></th><th>{{data.bonus.name}}</th><th>Primary</th><th>Secondary</th><th>MK</th><th>Maint.</th><th>Lvl.</th></tr>
+{{#data.bonus.levels}}
+<tr><td class="radio"><input type="radio" name="{{key}}"></td><td>{{amount}}</td><td>{{primary}}</td><td>{{secondary}}</td><td>{{martialKnowledge}}</td><td>{{maintainCost}}</td><td>{{minimumTechniqueLevel}}</td></tr>
+{{/data.bonus.levels}}
+</table>
+`;
+
+Template.selectOption = String.raw`
+<option value="{{value}}">{{name}}</option>
 `
+
+Template.disadvantage = String.raw`
+<tr><td><div class="disadvantageContainer">
+<div style="display:table; width:100%;">
+<select class="disadvantageSelector" style="display:table-cell; width:100%;"></select>
+<div class="space" style="display:table-cell; width:0.5rem;"/><div class="removeDisadvantage" style="display:table-cell; width:1px;">Remove</div>
+</div>
+</div></td></tr>
+`
+
